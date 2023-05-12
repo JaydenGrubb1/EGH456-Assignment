7ccaa586bb5f6d9001918737000b338bfd4e0d87
Jayden Grubb <jaydengrubb1@gmail.com>
2023-05-12 20:10:41 +1000

    Added touchscreen drivers and cleaned up code

--- Code/main.c
+++ Code/main.c
@@ -1,109 +1,90 @@
-/*
- * Copyright (c) 2015, Texas Instruments Incorporated
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- *
- * *  Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
- *
- * *  Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in the
- *    documentation and/or other materials provided with the distribution.
- *
- * *  Neither the name of Texas Instruments Incorporated nor the names of
- *    its contributors may be used to endorse or promote products derived
- *    from this software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
- * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
- * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
- * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
- * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
- * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
- * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
- * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
- * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
- * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
- * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
+/* Standard header files */
+#include <stdio.h>
+#include <stdint.h>
+#include <stdbool.h>
+#include <math.h>
 
-/*
- *  ======== empty_min.c ========
- */
-/* XDCtools Header files */
+/* XDCtools header files */
 #include <xdc/std.h>
+#include <xdc/runtime/System.h>
+#include <xdc/runtime/Types.h>
 
-/* BIOS Header files */
+/* BIOS header files */
 #include <ti/sysbios/BIOS.h>
 #include <ti/sysbios/knl/Task.h>
 
-/* TI-RTOS Header files */
-// #include <ti/drivers/EMAC.h>
+/* TI-RTOS header files */
 #include <ti/drivers/GPIO.h>
-// #include <ti/drivers/I2C.h>
-// #include <ti/drivers/SDSPI.h>
-// #include <ti/drivers/SPI.h>
-// #include <ti/drivers/UART.h>
-// #include <ti/drivers/USBMSCHFatFs.h>
-// #include <ti/drivers/Watchdog.h>
-// #include <ti/drivers/WiFi.h>
 
-/* Board Header file */
+/* Board header file */
 #include "Board.h"
 
-#define TASKSTACKSIZE   512
+/* GRLib header files */
+#include "grlib/grlib.h"
+#include "grlib/widget.h"
+#include "grlib/canvas.h"
 
-Task_Struct task0Struct;
-Char task0Stack[TASKSTACKSIZE];
+/* Driver header files */
+#include "drivers/Kentec320x240x16_ssd2119_spi.h"
+#include "drivers/touch.h"
 
-/*
- *  ======== heartBeatFxn ========
- *  Toggle the Board_LED0. The Task_sleep is determined by arg0 which
- *  is configured for the heartBeat Task instance.
+/* Global defines */
+#define TASK_STACK_SIZE 1024
+
+/* Task structs */
+Task_Struct g_sWidgetMsgQueueTask;
+
+/* Task stacks */
+char ga_cWidgetMsgQueueStack[TASK_STACK_SIZE];
+
+/* Global variables */
+tContext g_sContext;
+tRectangle g_sScreenRect;
+
+/**
+ * @brief Handles processing the messages for the widget message queue
+ *
+ * @param arg0 Unused
+ * @param arg1 Unused
  */
-Void heartBeatFxn(UArg arg0, UArg arg1)
-{
+void widgetMsgQueueFxn(UArg arg0, UArg arg1) {
 	while (1) {
-		Task_sleep((unsigned int)arg0);
-		GPIO_toggle(Board_LED0);
+		WidgetMessageQueueProcess();
 	}
 }
 
-/*
- *  ======== main ========
+/**
+ * @brief Application entry point
+ * 
+ * @return Unused
  */
-int main(void)
-{
-	Task_Params taskParams;
-
+int main(void) {
 	/* Call board init functions */
 	Board_initGeneral();
-	// Board_initEMAC();
 	Board_initGPIO();
-	// Board_initI2C();
-	// Board_initSDSPI();
-	// Board_initSPI();
-	// Board_initUART();
-	// Board_initUSB(Board_USBDEVICE);
-	// Board_initUSBMSCHFatFs();
-	// Board_initWatchdog();
-	// Board_initWiFi();
 
-	/* Construct heartBeat Task  thread */
+	/* Initialize touch screen */
+	Types_FreqHz cpuFreq;
+	BIOS_getCpuFreq(&cpuFreq);
+	Kentec320x240x16_SSD2119Init(cpuFreq.lo);
+	GrContextInit(&g_sContext, &g_sKentec320x240x16_SSD2119);
+	TouchScreenInit(cpuFreq.lo);
+	TouchScreenCallbackSet(WidgetPointerMessage);
+
+	/* Initialize screen rect */
+	g_sScreenRect.i16XMin = 0;
+	g_sScreenRect.i16YMin = 0;
+	g_sScreenRect.i16XMax = GrContextDpyWidthGet(&g_sContext);
+	g_sScreenRect.i16YMax = GrContextDpyHeightGet(&g_sContext);
+
+	/* Construct task threads */
+	Task_Params taskParams;
 	Task_Params_init(&taskParams);
-	taskParams.arg0 = 500;
-	taskParams.stackSize = TASKSTACKSIZE;
-	taskParams.stack = &task0Stack;
-	Task_construct(&task0Struct, (Task_FuncPtr)heartBeatFxn, &taskParams, NULL);
-
-	/* Turn on user LED  */
-	GPIO_write(Board_LED0, Board_LED_ON);
+	taskParams.stackSize = TASK_STACK_SIZE;
+	taskParams.stack = &ga_cWidgetMsgQueueStack;
+	Task_construct(&g_sWidgetMsgQueueTask, (Task_FuncPtr)widgetMsgQueueFxn, &taskParams, NULL);
 
 	/* Start BIOS */
 	BIOS_start();
-
 	return (0);
 }
