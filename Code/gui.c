2c283f772820bbe8728e8a19f93f5b6aa93642f0
Jayden Grubb <jaydengrubb1@gmail.com>
2023-05-15 18:32:38 +1000

    Added 2nd option panel to settings

--- Code/gui.c
+++ Code/gui.c
@@ -360,7 +360,7 @@ Canvas(
 Canvas(
 	g_sSettingsOption1Panel,																												   // struct name
 	&g_sSettingsContent,																													   // parent widget pointer
-	NULL,																																	   // sibling widget pointer
+	&g_sSettingsOption2Panel,																												   // sibling widget pointer
 	&g_sSettingsOption1DownBtn,																												   // child widget pointer
 	DISPLAY,																																   // display device pointer
 	140,																																	   // x position
@@ -421,6 +421,71 @@ RectangularButton(
 	0,												  // auto repeat delay
 	0,												  // auto repeat rate
 	OnSettingsOption1UpBtnClick						  // on-click function pointer
+);
+Canvas(
+	g_sSettingsOption2Panel,																												   // struct name
+	&g_sSettingsContent,																													   // parent widget pointer
+	NULL,																																	   // sibling widget pointer
+	&g_sSettingsOption2DownBtn,																												   // child widget pointer
+	DISPLAY,																																   // display device pointer
+	140,																																	   // x position
+	64,																																		   // y position
+	116,																																	   // width
+	52,																																		   // height
+	CANVAS_STYLE_FILL | CANVAS_STYLE_OUTLINE | CANVAS_STYLE_TEXT | CANVAS_STYLE_TEXT_HCENTER | CANVAS_STYLE_TEXT_TOP | CANVAS_STYLE_APP_DRAWN, // style
+	ClrBlack,																																   // fill color
+	ClrWhite,																																   // outline color
+	ClrWhite,																																   // text color
+	&g_sFontNf10,																															   // font pointer
+	"Option 2",																																   // text
+	NULL,																																	   // image pointer
+	OnSettingsOption2Paint																													   // on-paint function pointer
+);
+RectangularButton(
+	g_sSettingsOption2DownBtn,						  // struct name
+	&g_sSettingsOption2Panel,						  // parent widget pointer
+	&g_sSettingsOption2UpBtn,						  // sibling widget pointer
+	NULL,											  // child widget pointer
+	DISPLAY,										  // display device pointer
+	82,												  // x position
+	64,												  // y position
+	52,												  // width
+	52,												  // height
+	PB_STYLE_OUTLINE | PB_STYLE_TEXT | PB_STYLE_FILL, // style
+	ClrGray,										  // fill color
+	ClrGray,										  // press fill color
+	ClrWhite,										  // outline color
+	ClrWhite,										  // text color
+	&g_sFontNf36,									  // font pointer
+	"-",											  // text
+	NULL,											  // image pointer
+	NULL,											  // press image pointer
+	0,												  // auto repeat delay
+	0,												  // auto repeat rate
+	OnSettingsOption2DownBtnClick					  // on-click function pointer
+);
+RectangularButton(
+	g_sSettingsOption2UpBtn,						  // struct name
+	&g_sSettingsOption2Panel,						  // parent widget pointer
+	NULL,											  // sibling widget pointer
+	NULL,											  // child widget pointer
+	DISPLAY,										  // display device pointer
+	262,											  // x position
+	64,												  // y position
+	52,												  // width
+	52,												  // height
+	PB_STYLE_OUTLINE | PB_STYLE_TEXT | PB_STYLE_FILL, // style
+	ClrGray,										  // fill color
+	ClrGray,										  // press fill color
+	ClrWhite,										  // outline color
+	ClrWhite,										  // text color
+	&g_sFontNf36,									  // font pointer
+	"+",											  // text
+	NULL,											  // image pointer
+	NULL,											  // press image pointer
+	0,												  // auto repeat delay
+	0,												  // auto repeat rate
+	OnSettingsOption2UpBtnClick						  // on-click function pointer
 );
 
 /* Graph panel widget constructors */
@@ -681,6 +746,54 @@ void OnSettingsOption1DownBtnClick(tWidget *psWidget) {
 }
 
 void OnSettingsOption1Paint(tWidget *psWidget, tContext *psContext) {
+	GrContextForegroundSet(psContext, ClrRed);
+	GrContextFontSet(psContext, &g_sFontNf24);
+	// char text[8];
+	// snprintf(text, 8, "%03d RPM\0", g_i16CurrentSpeed);
+	GrStringDrawCentered(psContext, "test", -1, 198, 32, false);
+}
+
+void OnSettingsOption2UpBtnClick(tWidget *psWidget) {
+	// TODO: Implement
+}
+
+void OnSettingsOption2DownBtnClick(tWidget *psWidget) {
+	// TODO: Implement
+}
+
+void OnSettingsOption2Paint(tWidget *psWidget, tContext *psContext) {
+	GrContextForegroundSet(psContext, ClrRed);
+	GrContextFontSet(psContext, &g_sFontNf24);
+	// char text[8];
+	// snprintf(text, 8, "%03d RPM\0", g_i16CurrentSpeed);
+	GrStringDrawCentered(psContext, "test", -1, 198, 90, false);
+}
+
+void OnSettingsOption3UpBtnClick(tWidget *psWidget) {
+	// TODO: Implement
+}
+
+void OnSettingsOption3DownBtnClick(tWidget *psWidget) {
+	// TODO: Implement
+}
+
+void OnSettingsOption3Paint(tWidget *psWidget, tContext *psContext) {
+	GrContextForegroundSet(psContext, ClrRed);
+	GrContextFontSet(psContext, &g_sFontNf24);
+	// char text[8];
+	// snprintf(text, 8, "%03d RPM\0", g_i16CurrentSpeed);
+	GrStringDrawCentered(psContext, "test", -1, 198, 32, false);
+}
+
+void OnSettingsOption4UpBtnClick(tWidget *psWidget) {
+	// TODO: Implement
+}
+
+void OnSettingsOption4DownBtnClick(tWidget *psWidget) {
+	// TODO: Implement
+}
+
+void OnSettingsOption4Paint(tWidget *psWidget, tContext *psContext) {
 	GrContextForegroundSet(psContext, ClrRed);
 	GrContextFontSet(psContext, &g_sFontNf24);
 	// char text[8];
