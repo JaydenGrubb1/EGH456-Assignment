acfe18eab12398ae38af7177094e2c721413d65b
Jayden Grubb <jaydengrubb1@gmail.com>
2023-05-16 23:02:50 +1000

    Updated util time conversion functions documentation

--- Code/util.h
+++ Code/util.h
@@ -2,19 +2,19 @@
 #include <stdint.h>
 
 /**
- * @brief Convert a time to ticks
+ * @brief Convert the time given in hours, minutes and seconds to ticks (total seconds)
  * 
  * @param hour The hour portion of the time
  * @param minute The minute portion of the time
  * @param second The second portion of the time
- * @return The number of ticks
+ * @return The number of ticks (total seconds)
  */
 uint32_t TimeToTicks(uint8_t hour, uint8_t minute, uint8_t second);
 
 /**
- * @brief Convert ticks to a time
+ * @brief Convert ticks (total seconds) to time given in hours, minutes and seconds
  * 
- * @param ticks The number of ticks
+ * @param ticks The number of ticks (total seconds)
  * @param hour The hour portion of the time
  * @param minute The minute portion of the time
  * @param second The second portion of the time
