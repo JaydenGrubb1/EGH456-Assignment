5022b2b17ce3442637f1fa53fff725077d07745c
Mick Batchelor <mickbatch98@gmail.com>
2023-05-22 19:46:44 +1000

    fixed some build errors - SW_ROOT path had a different version of the tivaware libraries - added headers for drivers - added motorlib.lib to the library links

--- Code/Board.h
+++ Code/Board.h
@@ -60,6 +60,10 @@ extern "C" {
 #define Board_BUTTON0               EK_TM4C1294XL_USR_SW1
 #define Board_BUTTON1               EK_TM4C1294XL_USR_SW2
 
+#define Board_MOTOR_HALL_A          EK_TM4C1294XL_DRV832X_HALL_A_EDGES
+#define Board_MOTOR_HALL_B          EK_TM4C1294XL_DRV832X_HALL_B_EDGES
+#define Board_MOTOR_HALL_C          EK_TM4C1294XL_DRV832X_HALL_C_EDGES
+
 #define Board_I2C0                  EK_TM4C1294XL_I2C7
 #define Board_I2C1                  EK_TM4C1294XL_I2C8
 #define Board_I2C_TMP               EK_TM4C1294XL_I2C7
